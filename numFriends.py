0651422da94277b27202a59f80f676299f5d21d3
James M <jwmarion@gmail.com>
2017-02-14 13:01:43 -0500

    added next practiceprograms

---
+++ numFriends.py
@@ -0,0 +1,27 @@
+#ex7
+
+class Person(object):
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.friends = []
+
+    def greet(self, other_person):
+        print 'Hello %s, I am %s!' % (other_person.name, self.name)
+
+    def print_contact_info(self):
+        print "%s's email: %s %s's number: %s" % (self.name, self.email, self.name, self.phone)
+
+    def add_friend(self, friend):
+        self.friends.append(friend)
+
+    def num_friends(self):
+        print len(self.friends)
+
+sonny = Person("Sonny","sonny@hotmail.com", "483-485-4948")
+jordan = Person("Jordan", "jordan@aol.com", "495-586-3456")
+
+sonny.add_friend(jordan)
+
+sonny.num_friends()
